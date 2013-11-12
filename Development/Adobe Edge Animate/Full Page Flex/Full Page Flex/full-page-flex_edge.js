c8ee2f8313b36ab7bc80638ffd100b1dfcdb9e55
Chad Shryock <cdshryock@gannett.com>
2013-11-12 13:40:15 -0500

    Full Page Flex template

---
+++ Development/Adobe Edge Animate/Full Page Flex/Full Page Flex/full-page-flex_edge.js
@@ -0,0 +1,249 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+
+
+var resources = [
+];
+var symbols = {
+"stage": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+         dom: [
+],
+         symbolInstances: [
+
+         ]
+      },
+   states: {
+      "Base State": {
+         "${_Stage}": [
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "min-width", '320px'],
+            ["style", "overflow", 'visible'],
+            ["style", "height", '100%'],
+            ["style", "max-width", '1280px'],
+            ["style", "width", '100%']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_240x240": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '240px'],
+            ["style", "width", '240px'],
+            ["style", "min-width", '240px'],
+            ["style", "max-width", '240px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_320x320": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '320px'],
+            ["style", "width", '320px'],
+            ["style", "min-width", '320px'],
+            ["style", "max-width", '320px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_405x405": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '405px'],
+            ["style", "width", '405px'],
+            ["style", "min-width", '405px'],
+            ["style", "max-width", '405px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_480x480": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '480px'],
+            ["style", "width", '480px'],
+            ["style", "min-width", '480px'],
+            ["style", "max-width", '480px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_600x600": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '600px'],
+            ["style", "width", '600px'],
+            ["style", "min-width", '600px'],
+            ["style", "max-width", '600px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+},
+"_768x768": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   },
+   states: {
+      "Base State": {
+         "${symbolSelector}": [
+            ["style", "height", '768px'],
+            ["style", "width", '768px'],
+            ["style", "min-width", '768px'],
+            ["style", "max-width", '768px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: false,
+         timeline: [
+         ]
+      }
+   }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-591991892");
---
+++ Development/Adobe Edge Animate/Full Page Flex/Full Page Flex/full-page-flex_edgeActions.js
@@ -0,0 +1,146 @@
+/***********************
+* Adobe Edge Animate Composition Actions
+*
+* Edit this file with caution, being careful to preserve 
+* function signatures and comments starting with 'Edge' to maintain the 
+* ability to interact with these actions from within Adobe Edge Animate
+*
+***********************/
+window.version = {
+	'templateType': 'full-page-flex',
+	'number': 0.1
+};
+(function($, Edge, compId){
+	var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+	//Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      Symbol.bindSymbolAction(compId, symbolName, "creationComplete", function(sym, e) {
+         // insert code to be run when the symbol is created here
+         var currentSymbol = null,
+             playing = false;
+			$(document).on('setup', function(event) {
+				change(event.message.width, event.message.height);
+				window.eventReady();
+			});
+			$(document).on('play', function() {
+				playing = true;
+				sym.play();
+				if (currentSymbol) {
+					currentSymbol.play();
+				}
+			});
+			$(document).on('stop', function() {
+				playing = false;
+				sym.stop();
+				if (currentSymbol) {
+					currentSymbol.stop();
+				}
+			});
+			$(document).on('resize', function(event) {
+				var w = event.message.width,
+					 h = event.message.height;
+				change(w, h);
+				if (playing && currentSymbol) {
+					currentSymbol.play();
+				}
+			});
+			function change(w, h) {
+				// remove current symbol
+				if (currentSymbol) {
+					currentSymbol.deleteSymbol();
+				}
+				// create new symbol
+				var newSymbol = '_240x240',
+					 size = 240;
+				if (w > 768 && h > 768) {
+					newSymbol = '_768x768';
+					size = 768;
+				} else if (w > 600 && h > 600) {
+					newSymbol = '_600x600';
+					size = 600;
+				} else if (w > 480 && h > 480) {
+					newSymbol = '_480x480';
+					size = 480;
+				} else if (w > 405 && h > 405) {
+					newSymbol = '_405x405';
+					size = 405;
+				} else if (w > 320 && h > 320) {
+					newSymbol = '_320x320';
+					size = 320;
+				}
+				var symObj = sym.createChildSymbol(newSymbol, "stage");
+				currentSymbol = symObj;
+
+				// center
+				var mt = (h - size) / 2;
+				symObj.getSymbolElement().css({
+					'margin': mt + 'px auto 0 auto'
+				});
+			}
+			
+			function complete() {
+				
+			}
+
+			yepnope({
+				load: [
+					'http://i.usatoday.net/adsolutions/templates/edge/event-model.js'
+				],
+				complete: complete
+			});
+      });
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+   //=========================================================
+   
+   //Edge symbol: '_240x240'
+   (function(symbolName) {   
+   
+   })("_240x240");
+   //Edge symbol end:'_240x240'
+
+   //=========================================================
+   
+   //Edge symbol: '_320x320'
+   (function(symbolName) {   
+   
+   })("_320x320");
+   //Edge symbol end:'_320x320'
+
+   //=========================================================
+   
+   //Edge symbol: '_405x405'
+   (function(symbolName) {   
+   
+   })("_405x405");
+   //Edge symbol end:'_405x405'
+
+   //=========================================================
+   
+   //Edge symbol: '_480x480'
+   (function(symbolName) {   
+   
+   })("_480x480");
+   //Edge symbol end:'_480x480'
+
+   //=========================================================
+   
+   //Edge symbol: '_600x600'
+   (function(symbolName) {   
+   
+   })("_600x600");
+   //Edge symbol end:'_600x600'
+
+   //=========================================================
+   
+   //Edge symbol: '_768x768'
+   (function(symbolName) {   
+   
+   })("_768x768");
+   //Edge symbol end:'_768x768'
+
+})(jQuery, AdobeEdge, "EDGE-591991892");
