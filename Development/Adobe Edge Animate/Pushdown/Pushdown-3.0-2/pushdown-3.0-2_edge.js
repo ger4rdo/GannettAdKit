7a3c10d93164e1cf168d7b0ec4f01d2954b310f9
Chad Shryock <cdshryock@gannett.com>
2014-06-11 09:32:08 -0400

    Removed the "not yet" message and added comments

---
+++ Development/Adobe Edge Animate/Pushdown/Pushdown-3.0-2/pushdown-3.0-2_edge.js
@@ -0,0 +1,64 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+var opts = {};
+var resources = [
+];
+var symbols = {
+"stage": {
+    version: "3.0.0",
+    minimumCompatibleVersion: "3.0.0",
+    build: "3.0.0.322",
+    baseState: "Base State",
+    scaleToFit: "both",
+    centerStage: "both",
+    initialState: "Base State",
+    gpuAccelerate: false,
+    resizeInstances: false,
+    content: {
+            dom: [
+],
+            symbolInstances: [
+
+            ]
+        },
+    states: {
+        "Base State": {
+            "${_Stage}": [
+                ["color", "background-color", 'rgba(255,255,255,1)'],
+                ["style", "min-width", '1629px'],
+                ["style", "overflow", 'hidden'],
+                ["style", "height", '1008px'],
+                ["style", "max-width", '2560px'],
+                ["style", "width", '2560px']
+            ]
+        }
+    },
+    timelines: {
+        "Default Timeline": {
+            fromState: "Base State",
+            toState: "",
+            duration: 0,
+            autoPlay: false,
+            timeline: [
+            ]
+        }
+    }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources, opts);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-101948563");
